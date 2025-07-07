4771759c6fce670ee6de07d418216d8f3438d30c
Peridan <danielsatelpereira@gmail.com>
2025-07-07 13:49:37 -0300

    feat(layout): atualiza header e footer com estrutura flexível e visual refinado

--- php/americanas-clone-php/footer.php
+++ php/americanas-clone-php/footer.php
@@ -1,6 +1,8 @@
-    <footer>
-        <p>Desenvolvido por Daniel Satel Pereira</p>
-    </footer>
-    </body>
+<footer>
+    <div class="container-footer">
+        <p>&copy; 2025 Desenvolvido por Daniel Satel Pereira</p>
+    </div>
+</footer>
+</body>
 
-    </html>
+</html>
--- php/americanas-clone-php/header.php
+++ php/americanas-clone-php/header.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Americanas - UniAlfa</title>
+    <title><?= $pageTitle ?? 'Americanas - UniAlfa' ?></title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" href="imagens/icone.webp">
+    <link rel="shortcut icon" href="imagens/icone.webp" type="image/webp">
 </head>
+
 <body>
     <header>
-        <a href="index.php" title="Home">
-            <img src="imagens/logo.png" alt="Americanas">
-        </a>
+        <div class="container-header">
+            <a href="index.php" title="Home">
+                <img src="imagens/logo.png" alt="Americanas" class="logo">
+            </a>
 
-        <nav>
-            <ul>
-                <li><a href="index.php" title="Home">Home</a></li>
-                <li><a href="sobre.php" title="Sobre">Sobre</a></li>
-                <li><a href="produtos.php" title="Produtos">Produtos</a></li>
-                <li><a href="contato.php" title="Contato">Contato</a></li>
-            </ul>
-        </nav>
+            <nav>
+                <ul class="nav-list">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="sobre.php">Sobre</a></li>
+                    <li><a href="produtos.php">Produtos</a></li>
+                    <li><a href="contato.php">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
