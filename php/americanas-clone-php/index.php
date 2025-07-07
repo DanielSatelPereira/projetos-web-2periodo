b9d0fc2fd886dddd92435e18acd8753679bdfafa
Peridan <danielsatelpereira@gmail.com>
2025-07-07 13:52:33 -0300

    feat: exibe produtos em destaque na homepage usando layout em grid

--- php/americanas-clone-php/index.php
+++ php/americanas-clone-php/index.php
@@ -1,35 +1,33 @@
 <?php
+$pageTitle = "Página Inicial";
 include "header.php";
 include "dados.php";
 ?>
 
 <main>
-    <h1>Produtos destaque:</h1>
+    <h1>Produtos em Destaque</h1>
+
     <div class="grid">
-
         <?php
-        //echo "<pre>";
-        //var_dump($produtos);
         $contador = 1;
-        shuffle($produtos);
-        foreach ($produtos as $produto) {
+        shuffle($produtos); // Embaralha os produtos
 
-
-            ?>
-            <!--trecho do HTML-->
+        foreach ($produtos as $produto):
+        ?>
             <div class="coluna">
                 <img src="imagens/<?= $produto["imagem"] ?>" alt="<?= $produto["nome"] ?>">
                 <p>
                     <strong><?= $produto["nome"] ?></strong><br>
                     R$ <?= $produto["valor"] ?>
                 </p>
-                <p><a href="produto.php?id=<?= $produto["id"] ?>">Detalhes</a>
+                <p>
+                    <a href="produto.php?id=<?= $produto["id"] ?>">Ver Detalhes</a>
                 </p>
             </div>
-            <?php
+        <?php
             $contador++;
             if ($contador > 4) break;
-        }
+        endforeach;
         ?>
     </div>
 </main>
