f355c82d3aa8c0cda254a3539600a64aa96b27cc
Peridan <danielsatelpereira@gmail.com>
2025-07-07 10:45:33 -0300

    Refatoração para a padronização do fomulário

---
+++ php/americanas-clone-php/formulario.php
@@ -0,0 +1,14 @@
+<?php include "header.php"; ?>
+
+<main class="container">
+    <h2>Verificação de Idade</h2>
+
+    <form action="condicoes.php" method="get">
+        <label for="idade">Digite a sua idade:</label>
+        <input type="number" id="idade" name="idade" required min="0" placeholder="Ex: 18">
+
+        <button type="submit">Enviar</button>
+    </form>
+</main>
+
+<?php include "footer.php"; ?>
